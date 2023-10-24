a31567cd24a42661ad3c61aa25250c2622b965fb
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2023-10-24 13:54:57 -0400

    finished routes and controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,15 @@ class ProductsController < ApplicationController
     first = Product.first
     render json: (first)
   end
+
+  def product_all
+    all = Product.all
+    render json: (all)
+  end
+
+  def sample3
+    sample1 = Product.all
+    sample2 = sample1.sample
+    render json: (sample2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/product", controller: "products", action: "product"
+  get "/products", controller: "products", action: "product_all"
+  get "/rando", controller: "products", action: "sample3"
 end
