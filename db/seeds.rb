11f6943ca842fa2c81dd4add1a3a839edffcfb0e
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2023-10-24 13:05:06 -0400

    built json builder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-products = Product.create({ name: "Spoon", price: 10, image_url: "www.google.com", description: "spoon" })
-products = Product.create({ name: "Cutting Board", price: 30, image_url: "www.google.com", description: "cutting board" })
+product1 = Product.create(name: "Spoon", price: 10, image_url: "www.google.com", description: "spoon")
+product2 = Product.create(name: "Cutting Board", price: 30, image_url: "www.google.com", description: "cutting board")
+product3 = Product.create(name: "Puzzle", price: 30, image_url: "www.google.com", description: "puzzle")
