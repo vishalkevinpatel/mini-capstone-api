c853e5b6b77ae25950645296d04a6be9dd6018ca
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2023-10-26 11:39:09 -0400

    cleaning up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "/products", controller: "products", action: "index"
   get "/products/:id" => "products#show"
+  get "/products", controller: "products", action: "index"
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
