dc418166794dfdec5a848acf182b2ec27ab6491b
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2023-10-31 14:39:58 -0400

    cleaned up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,26 @@
 #   {name: "bob", email: "bob@uso.com", phone_number: "6108675309"},
 #   {name: "lyle", email: "lyle@uso.com", phone_number: "4848675309"}
 # ])
+
 # Image.create!([
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "christmas tree", price: "45.0", image_url: "https://media.istockphoto.com/id/1346236801/vector/vector-christmas-tree-isolated-on-transparent-background-beautiful-shining-christmas-tree.jpg?s=2048x2048&w=is&k=20&c=5Lhx96ujxx35zorGiZpmVirfvs9EpfBPvK7DX5xL1XA=", description: "christmas tree", inventory: nil, supplier_id: 2},
-#   {name: "Spoon", price: "10.0", image_url: "https://www.pngmart.com/files/1/Wooden-Spoon-PNG-Clipart.png", description: "spoon", inventory: nil, supplier_id: 1},
-#   {name: "Spoon", price: "10.0", image_url: "https://www.pngmart.com/files/1/Wooden-Spoon-PNG-Clipart.png", description: "spoon", inventory: nil, supplier_id: 1},
-#   {name: "Spoon", price: "10.0", image_url: "https://www.pngmart.com/files/1/Wooden-Spoon-PNG-Clipart.png", description: "spoon", inventory: nil, supplier_id: 1},
-#   {name: "spaghetti", price: "300.0", image_url: "https://pngimg.com/uploads/spaghetti/spaghetti_PNG24.png", description: "idk", inventory: nil, supplier_id: 1},
-#   {name: "Cutting Board", price: "30.0", image_url: "https://www.woodcuttingboards.com/cms/controller/services/Thumbnail.ashx?width=595&fileName=/media/ecommerce_option.photo/CBU1069.png", description: "cutting board", inventory: nil, supplier_id: 1},
-#   {name: "Cutting Board", price: "35.0", image_url: "https://www.woodcuttingboards.com/cms/controller/services/Thumbnail.ashx?width=595&fileName=/media/ecommerce_option.photo/CBU1069.png", description: "cutting board", inventory: nil, supplier_id: 1},
-#   {name: "Puzzle", price: "30.0", image_url: "https://www.uglyducklinghouse.com/wp-content/uploads/2019/04/DIY-wooden-elephant-puzzle-12.jpg", description: "puzzle", inventory: nil, supplier_id: 1},
-#   {name: "scoop", price: "14.0", image_url: "https://www.burkedecor.com/cdn/shop/products/TKR12-GPS_1800x1800.jpg?v=1571713114", description: "its not a scoop", inventory: nil, supplier_id: 1}
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "christmas tree", price: "45.0", description: "christmas tree", inventory: nil, supplier_id: 2},
+#   {name: "Spoon", price: "10.0", description: "spoon", inventory: nil, supplier_id: 1},
+#   {name: "Spoon", price: "10.0", description: "spoon", inventory: nil, supplier_id: 1},
+#   {name: "Spoon", price: "10.0", description: "spoon", inventory: nil, supplier_id: 1},
+#   {name: "spaghetti", price: "300.0", description: "idk", inventory: nil, supplier_id: 1},
+#   {name: "Cutting Board", price: "30.0", description: "cutting board", inventory: nil, supplier_id: 1},
+#   {name: "Cutting Board", price: "35.0", description: "cutting board", inventory: nil, supplier_id: 1},
+#   {name: "Puzzle", price: "30.0", description: "puzzle", inventory: nil, supplier_id: 1},
+#   {name: "scoop", price: "14.0", description: "its not a scoop", inventory: nil, supplier_id: 1}
 # ])
 
 # Image.create!([
